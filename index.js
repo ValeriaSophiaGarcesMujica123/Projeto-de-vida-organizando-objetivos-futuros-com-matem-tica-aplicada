6a1c26bff690da30192199423deb9748114a5f21
Valeria <valeria.mujica@escola.pr.gov.br>
2026-05-28 08:48:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+// Selecionando os elementos da página pelos IDs
+const homeSection = document.getElementById('home-section');
+const detailsSection = document.getElementById('details-section');
+const btnEnter = document.getElementById('btn-enter');
+const btnBack = document.getElementById('btn-back');
+
+// Função para entrar na tela de detalhes
+btnEnter.addEventListener('click', function() {
+    // Esconde a tela inicial
+    homeSection.classList.remove('active');
+    homeSection.classList.add('hide');
+    
+    // Mostra a tela de detalhes
+    detailsSection.classList.remove('hide');
+    detailsSection.classList.add('active');
+});
+
+// Função para voltar para a tela inicial
+btnBack.addEventListener('click', function() {
+    // Esconde a tela de detalhes
+    detailsSection.classList.remove('active');
+    detailsSection.classList.add('hide');
+    
+    // Mostra a tela inicial
+    homeSection.classList.remove('hide');
+    homeSection.classList.add('active');
+});
