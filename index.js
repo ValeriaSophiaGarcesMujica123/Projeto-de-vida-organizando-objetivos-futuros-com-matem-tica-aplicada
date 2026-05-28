4244b8a03fef4ba1b9fe4d23c7681526c4b9d370
Valeria <valeria.mujica@escola.pr.gov.br>
2026-05-28 08:51:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 // Selecionando os elementos da página pelos IDs
-const homeSection = document.getElementById('home-section');
-const detailsSection = document.getElementById('details-section');
-const btnEnter = document.getElementById('btn-enter');
-const btnBack = document.getElementById('btn-back');
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecionando os elementos da página pelos IDs
+    const homeSection = document.getElementById('home-section');
+    const detailsSection = document.getElementById('details-section');
+    const btnEnter = document.getElementById('btn-enter');
+    const btnBack = document.getElementById('btn-back');
 
-// Função para entrar na tela de detalhes
-btnEnter.addEventListener('click', function() {
-    // Esconde a tela inicial
-    homeSection.classList.remove('active');
-    homeSection.classList.add('hide');
-    
-    // Mostra a tela de detalhes
-    detailsSection.classList.remove('hide');
-    detailsSection.classList.add('active');
-});
+    // Função para entrar na tela de detalhes
+    if (btnEnter) {
+        btnEnter.addEventListener('click', function() {
+            homeSection.classList.remove('active');
+            homeSection.classList.add('hide');
+            
+            detailsSection.classList.remove('hide');
+            detailsSection.classList.add('active');
+        });
+    }
 
-// Função para voltar para a tela inicial
-btnBack.addEventListener('click', function() {
-    // Esconde a tela de detalhes
-    detailsSection.classList.remove('active');
-    detailsSection.classList.add('hide');
-    
-    // Mostra a tela inicial
-    homeSection.classList.remove('hide');
-    homeSection.classList.add('active');
+    // Função para voltar para a tela inicial
+    if (btnBack) {
+        btnBack.addEventListener('click', function() {
+            detailsSection.classList.remove('active');
+            detailsSection.classList.add('hide');
+            
+            homeSection.classList.remove('hide');
+            homeSection.classList.add('active');
+        });
+    }
 });
